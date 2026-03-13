4adaf78ebc83ac55b6bfdda6be4c8cceef526762
Yoo Sihun <yosi475@naver.com>
2026-03-13 09:20:52 +0900

    Fix printf format specifiers in 1-1.c
    Updated printf format specifiers for various data types.

--- code/1-1.c
+++ code/1-1.c
@@ -2,6 +2,21 @@
 
 int main()
 {
+    int var1 = 100;
+    printf("int : %?\n", var1);
+    printf("hex : %?\n", var1);
+    printf("oct : %?\n", var1);
 
+    char var2 = 'c';
+    printf("char : %?\n", var2);
+
+    char* var3 = "hello world!";
+    printf("string : %?\n", var3);
+
+    float var4 = 3.14592;
+    printf("float : %?\n", var4);
+    printf("exp : %?\n", var4);
+
+    
     return 0;
 }
