9fea9fa6f2a5e7e2f9fce1025a6abae22549d10a
Yoo Sihun <yosi475@naver.com>
2026-03-13 10:07:24 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -3,19 +3,19 @@
 int main()
 {
     int var1 = 100;
-    printf("int : %?\n", var1);
-    printf("hex : %?\n", var1);
-    printf("oct : %?\n", var1);
+    printf("int : %d\n", var1);
+    printf("hex : %d\n", var1);
+    printf("oct : %d\n", var1);
 
     char var2 = 'c';
-    printf("char : %?\n", var2);
+    printf("char : %c\n", var2);
 
     char* var3 = "hello world!";
-    printf("string : %?\n", var3);
+    printf("string : %s\n", var3);
 
     float var4 = 3.14592;
-    printf("float : %?\n", var4);
-    printf("exp : %?\n", var4);
+    printf("float : %f\n", var4);
+    printf("exp : %lf\n", var4);
 
     
     return 0;
