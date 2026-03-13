1484e13879b8614e624c7c77328faba6920d3666
Yoo Sihun <yosi475@naver.com>
2026-03-13 09:38:18 +0900

    Correct variable types and address syntax
    Fix variable declarations and address casting.

--- code/2-3.c
+++ code/2-3.c
@@ -2,7 +2,15 @@
 
 int main()
 {
+    usigned int x = 0x12345678;
 
+    usinged char* addr = (unsigned char*)8x;
+
+    printf("%x\n", *addr);
+    printf("%x\n", *(addr + 1));
+    printf("%x\n", *(addr + 2));
+    printf("%x\n", *(addr + 3));
+    
     return 0;
 }
 
