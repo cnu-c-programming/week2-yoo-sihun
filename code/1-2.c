d0a7ef7d2cdcf2f891843167efd7ea9eb1129e54
Yoo Sihun <yosi475@naver.com>
2026-03-13 22:33:52 +0900

    Fix formatting in printf statements

--- code/1-2.c
+++ code/1-2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    printf("% 10s % 10s % 10s\n", "name", "id", "value");
-    printf("% 10s % 10d % 10.2f\n", "kim", 12, 4.12);
-    printf("% 10s % 10d % 10.2f\n", "lee", 1922, 3.21);
-    printf("% 10s % 10d % 10.2f\n", "park", 432, 1.0);
+    printf("%10s %10s %10s\n", "name", "id", "value");
+    printf("%10s %10d %10.2f\n", "kim", 12, 4.12);
+    printf("%10s %10d %10.2f\n", "lee", 1922, 3.21);
+    printf("%10s %10d %10.2f\n", "park", 432, 1.0);
 
     return 0;
 }
